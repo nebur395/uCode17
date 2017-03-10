d875dee2d32528e79eabec9459a6d99031ddd865
Nicu1309 <nicu1309@gmail.com>
2017-03-10 22:04:59 +0100

    Initial scalatra structure

---
+++ src/main/scala/com/machine/lenin/MyScalatraServlet.scala
@@ -0,0 +1,16 @@
+package com.machine.lenin
+
+import org.scalatra._
+
+class MyScalatraServlet extends MlStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
