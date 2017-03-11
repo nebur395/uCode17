b70ee68d4bd04a70e1ed4b357c659ea33e1b6e84
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-03-11 07:47:54 +0100

    Delete fucking machin lenin

--- src/main/webapp/javascript/controllers/starterController.js
+++ src/main/webapp/javascript/controllers/starterController.js
@@ -1,29 +1,5 @@
 angular.module('machinLenin')
 
     .controller('starterCtrl', ['$scope', '$state', 'videoFilter', function ($scope, $state, videoFilter) {
-        // DRAG & DROP SECTION
-        $scope.$on('$dropletReady', function whenDropletReady() {
-            $scope.interface.allowedExtensions(['png', 'mov']);
-        });
 
-        // VIDEO FILTERS
-        $scope.jazzy = false;
-        $scope.vignette = false;
-        $scope.sepia = false;
-        $scope.whiteBlack = false;
-
-        // VIEW MANAGEMENT
-        $scope.firstView = true;
-        $scope.secondView = false;
-        $scope.thirdView = false;
-
-        $scope.sendFilters = function () {
-            var filters = [
-                $scope.jazzy,
-                $scope.vignette,
-                $scope.sepia,
-                $scope.whiteBlack
-            ];
-            videoFilter.sendFilters(filters, callback);
-        }
     }]);
